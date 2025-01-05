1936c4f816441200f51467cc0ace9848422c960c
knishk <96434556+k4ndy-1@users.noreply.github.com>
2025-01-05 18:14:07 +0530

    Update abc.py

--- abc.py
+++ abc.py
@@ -6,51 +6,14 @@ from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 import pandas as pd
-from selenium import webdriver
-from selenium.webdriver.chrome.options import Options
-from webdriver_manager.chrome import ChromeDriverManager
-
-import streamlit as st
-
-from selenium import webdriver
-from webdriver_manager.chrome import ChromeDriverManager
-from selenium.webdriver.chrome.service import Service
-import time
-from bs4 import BeautifulSoup
-
-# ------------- Settings for Pages -----------
-st.set_page_config(layout="wide")
-
-# Keep text only
-def get_website_content(url):
-    driver = None
-    try:
-        # Using on Local
-        options = webdriver.ChromeOptions()
-        options.add_argument('--headless')
-        options.add_argument('--disable-gpu')
-        options.add_argument('--window-size=1920,1200')
-        driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()),
-                                  options=options)
-        st.write(f"DEBUG:DRIVER:{driver}")
-        driver.get(url)
-        time.sleep(5)
-        html_doc = driver.page_source
-        driver.quit()
-        soup = BeautifulSoup(html_doc, "html.parser")
-        return soup.get_text()
-    except Exception as e:
-        st.write(f"DEBUG:INIT_DRIVER:ERROR:{e}")
-    finally:
-        if driver is not None: driver.quit()
-    return None
-
 
 # Function to get top colleges based on stream and city
 def get_top_colleges(stream, city):
+    # Set up Selenium WebDriver (make sure the driver is in the PATH or specify the path)
+    driver_path = './chromedriver.exe'  # Replace this with the path to your ChromeDriver executable
 
     # Setup Service for WebDriver
-    service = Service(get_driver)
+    service = Service(driver_path)
     
     # Optionally, set some options for the browser (headless mode, etc.)
     options = Options()
